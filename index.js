a4e3784d18d15d76adf93f44dec70054574d83a1
sunnidaze7 <pnykii98@gmail.com>
2023-06-30 20:40:05 -0500

    added associations and complete

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const { Band } = require('./models/Band')
 const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
+//band has multiple musicians
+Band.hasOne(Musician)
+Musician.hasOne(Band)
 
-
+//bands have many songs and vice versa
+Band.belongsToMany(Song, {through: "band-song"})
+Song.belongsToMany(Band, {through: "band-song"})
 
 module.exports = {
     Band,
